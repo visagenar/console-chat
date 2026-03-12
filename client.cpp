7463ed04ea5cbad0f3afe1137f6451e422cf0ed8
naran <ytfake007@gmail.com>
2026-03-12 21:23:21 +0300

    Update client.cpp

--- client.cpp
+++ client.cpp
@@ -1,213 +1,125 @@
-#include "common.h"
-#include <conio.h>
+#include <iostream>
+#include <string>
+#include <thread>
+#include <atomic>
+#include <cstring>
+#include <cstdlib>
 
-std::atomic<bool> clientRunning(true);
-std::string username;
+#ifdef _WIN32
+#include <winsock2.h>
+#include <ws2tcpip.h>
+#include <windows.h>
+#pragma comment(lib, "ws2_32.lib")
+#else
+#error "This code is for Windows only (MinGW)"
+#endif
 
-void receiveMessages(SOCKET serverSocket) {
-    char recvbuf[DEFAULT_BUFLEN];
-    PacketHeader header;
-    
-    while (clientRunning) {
-        int result = receiveData(serverSocket, reinterpret_cast<char*>(&header), sizeof(PacketHeader));
-        if (result <= 0) {
-            std::cout << "\nConnection to server lost!" << std::endl;
-            clientRunning = false;
+const int PORT = 8888;
+const int BUFFER_SIZE = 4096;
+
+std::atomic<bool> client_running(true);
+SOCKET sock;
+
+void receiveMessages() {
+    char buffer[BUFFER_SIZE];
+    while (client_running) {
+        uint32_t net_len;
+        int ret = recv(sock, (char*)&net_len, sizeof(net_len), 0);
+        if (ret <= 0) {
+            if (client_running) std::cout << "Disconnected from server." << std::endl;
+            client_running = false;
             break;
         }
-        
-        if (header.dataLength > 0) {
-            result = receiveData(serverSocket, recvbuf, header.dataLength);
-            if (result <= 0) {
-                std::cout << "\nConnection to server lost!" << std::endl;
-                clientRunning = false;
-                break;
-            }
-            recvbuf[header.dataLength] = '\0';
+        uint32_t msg_len = ntohl(net_len);
+        if (msg_len >= BUFFER_SIZE) continue;
+        ret = recv(sock, buffer, msg_len, 0);
+        if (ret <= 0) {
+            if (client_running) std::cout << "Disconnected from server." << std::endl;
+            client_running = false;
+            break;
         }
-        
-        switch (header.type) {
-            case MSG_PUBLIC:
-            case MSG_PRIVATE:
-                std::cout << "\n" << recvbuf << std::endl;
-                std::cout << "[" << getCurrentTime() << "] You: ";
-                fflush(stdout);
-                break;
-                
-            case MSG_LIST_USERS:
-                std::cout << "\n" << recvbuf << std::endl;
-                std::cout << "[" << getCurrentTime() << "] You: ";
-                fflush(stdout);
-                break;
-                
-            case MSG_ERROR:
-                std::cout << "\n[ERROR] " << recvbuf << std::endl;
-                std::cout << "[" << getCurrentTime() << "] You: ";
-                fflush(stdout);
-                break;
-                
-            case MSG_SUCCESS:
-                std::cout << "\n[SUCCESS] " << recvbuf << std::endl;
-                std::cout << "[" << getCurrentTime() << "] You: ";
-                fflush(stdout);
-                break;
-                
-            default:
-                break;
-        }
+        buffer[msg_len] = '\0';
+        std::string msg(buffer, msg_len);
+        std::cout << msg << std::endl;
     }
 }
 
-void printHelp() {
-    std::cout << "\n=== Chat Commands ===" << std::endl;
-    std::cout << "/help - Show this help" << std::endl;
-    std::cout << "/users - Show active users" << std::endl;
-    std::cout << "/msg <username> <text> - Send private message" << std::endl;
-    std::cout << "/quit - Exit chat" << std::endl;
-    std::cout << "Any other text - Send public message" << std::endl;
-    std::cout << "===================\n" << std::endl;
-}
+void sendMessages(const std::string& name) {
+    uint32_t name_len = htonl(name.size());
+    send(sock, (char*)&name_len, sizeof(name_len), 0);
+    send(sock, name.c_str(), name.size(), 0);
 
-int main() {
-    WSADATA wsaData;
-    SOCKET serverSocket = INVALID_SOCKET;
-    struct addrinfo *result = NULL, *ptr = NULL, hints;
-    
-    // Инициализация Winsock
-    int iResult = WSAStartup(MAKEWORD(2, 2), &wsaData);
-    if (iResult != 0) {
-        std::cout << "WSAStartup failed: " << iResult << std::endl;
-        return 1;
-    }
-    
-    ZeroMemory(&hints, sizeof(hints));
-    hints.ai_family = AF_UNSPEC;
-    hints.ai_socktype = SOCK_STREAM;
-    hints.ai_protocol = IPPROTO_TCP;
-    
-    std::string serverAddress;
-    std::cout << "Enter server address (default localhost): ";
-    std::getline(std::cin, serverAddress);
-    if (serverAddress.empty()) {
-        serverAddress = "localhost";
-    }
-    
-    iResult = getaddrinfo(serverAddress.c_str(), DEFAULT_PORT, &hints, &result);
-    if (iResult != 0) {
-        std::cout << "getaddrinfo failed: " << iResult << std::endl;
-        WSACleanup();
-        return 1;
-    }
-    
-    for (ptr = result; ptr != NULL; ptr = ptr->ai_next) {
-        serverSocket = socket(ptr->ai_family, ptr->ai_socktype, ptr->ai_protocol);
-        if (serverSocket == INVALID_SOCKET) {
-            continue;
-        }
-        
-        iResult = connect(serverSocket, ptr->ai_addr, (int)ptr->ai_addrlen);
-        if (iResult == SOCKET_ERROR) {
-            closesocket(serverSocket);
-            serverSocket = INVALID_SOCKET;
-            continue;
-        }
-        break;
-    }
-    
-    freeaddrinfo(result);
-    
-    if (serverSocket == INVALID_SOCKET) {
-        std::cout << "Unable to connect to server!" << std::endl;
-        WSACleanup();
-        return 1;
-    }
-    
-    // Вход в чат
-    std::cout << "Connected to server!" << std::endl;
-    std::cout << "Enter your username: ";
-    std::getline(std::cin, username);
-    
-    if (!sendMessage(serverSocket, MSG_LOGIN, username)) {
-        std::cout << "Failed to send login info" << std::endl;
-        closesocket(serverSocket);
-        WSACleanup();
-        return 1;
-    }
-    
-    // Ожидание подтверждения
-    PacketHeader header;
-    char recvbuf[DEFAULT_BUFLEN];
-    
-    iResult = receiveData(serverSocket, reinterpret_cast<char*>(&header), sizeof(PacketHeader));
-    if (iResult > 0 && header.dataLength > 0) {
-        iResult = receiveData(serverSocket, recvbuf, header.dataLength);
-        if (iResult > 0) {
-            recvbuf[header.dataLength] = '\0';
-            
-            if (header.type == MSG_ERROR) {
-                std::cout << "Login failed: " << recvbuf << std::endl;
-                closesocket(serverSocket);
-                WSACleanup();
-                return 1;
-            } else if (header.type == MSG_SUCCESS) {
-                std::cout << recvbuf << std::endl;
-            }
-        }
-    }
-    
-    std::cout << "\nWelcome to the chat, " << username << "!" << std::endl;
-    printHelp();
-    
-    // Запуск потока для получения сообщений
-    std::thread receiver(receiveMessages, serverSocket);
-    
-    // Отправка сообщений
     std::string input;
-    std::cout << "[" << getCurrentTime() << "] You: ";
-    
-    while (clientRunning) {
+    while (client_running) {
         std::getline(std::cin, input);
-        
-        if (!clientRunning) break;
-        
-        if (input.empty()) {
-            std::cout << "[" << getCurrentTime() << "] You: ";
-            continue;
-        }
-        
+        if (!client_running) break;
+
         if (input == "/quit") {
-            sendMessage(serverSocket, MSG_LOGOUT, "");
-            clientRunning = false;
+            uint32_t msg_len = htonl(input.size());
+            send(sock, (char*)&msg_len, sizeof(msg_len), 0);
+            send(sock, input.c_str(), input.size(), 0);
+            client_running = false;
             break;
         }
-        else if (input == "/help") {
-            printHelp();
-            std::cout << "[" << getCurrentTime() << "] You: ";
-        }
-        else if (input == "/users") {
-            sendMessage(serverSocket, MSG_LIST_USERS, "");
-        }
-        else if (input.substr(0, 5) == "/msg ") {
-            size_t space1 = input.find(' ', 5);
-            if (space1 != std::string::npos) {
-                std::string target = input.substr(5, space1 - 5);
-                std::string msg = input.substr(space1 + 1);
-                sendMessage(serverSocket, MSG_PRIVATE, target + " " + msg);
-            } else {
-                std::cout << "Usage: /msg <username> <message>" << std::endl;
-            }
-            std::cout << "[" << getCurrentTime() << "] You: ";
-        }
-        else {
-            sendMessage(serverSocket, MSG_PUBLIC, input);
-            std::cout << "[" << getCurrentTime() << "] You: ";
-        }
+
+        uint32_t msg_len = htonl(input.size());
+        send(sock, (char*)&msg_len, sizeof(msg_len), 0);
+        send(sock, input.c_str(), input.size(), 0);
     }
-    
-    receiver.detach();
-    closesocket(serverSocket);
+}
+
+int main(int argc, char* argv[]) {
+    SetConsoleOutputCP(65001);
+    SetConsoleCP(65001);
+    setlocale(LC_ALL, ".UTF-8");
+
+    if (argc < 2) {
+        std::cout << "Usage: client.exe <server_ip> [port]" << std::endl;
+        return 1;
+    }
+
+    std::string serverIP = argv[1];
+    int port = (argc >= 3) ? std::atoi(argv[2]) : PORT;
+
+    std::cout << "Enter your name: ";
+    std::string name;
+    std::getline(std::cin, name);
+
+    WSADATA wsaData;
+    if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0) {
+        std::cerr << "WSAStartup failed" << std::endl;
+        return 1;
+    }
+
+    sock = socket(AF_INET, SOCK_STREAM, 0);
+    if (sock == INVALID_SOCKET) {
+        std::cerr << "socket creation failed" << std::endl;
+        WSACleanup();
+        return 1;
+    }
+
+    sockaddr_in serverAddr;
+    serverAddr.sin_family = AF_INET;
+    inet_pton(AF_INET, serverIP.c_str(), &serverAddr.sin_addr);
+    serverAddr.sin_port = htons(port);
+
+    if (connect(sock, (sockaddr*)&serverAddr, sizeof(serverAddr)) == SOCKET_ERROR) {
+        std::cerr << "connection failed" << std::endl;
+        closesocket(sock);
+        WSACleanup();
+        return 1;
+    }
+
+    std::cout << "Connected to server. Type /quit to exit." << std::endl;
+
+    std::thread recvThread(receiveMessages);
+    std::thread sendThread(sendMessages, name);
+
+    recvThread.join();
+    sendThread.join();
+
+    closesocket(sock);
     WSACleanup();
-    
-    std::cout << "Disconnected from server." << std::endl;
+    std::cout << "Client stopped." << std::endl;
     return 0;
 }
